4879058fe3b6895cde372a1dc7a598e16d81c884
Pedro <pedrofernandezalvarez6@gmail.com>
2017-02-22 20:03:35 +0100

    Arreglado testFromTxt y todo funcionando desde maven

--- src/main/java/es/uniovi/asw/CitizensLoader.java
+++ src/main/java/es/uniovi/asw/CitizensLoader.java
@@ -14,63 +14,69 @@ import es.uniovi.asw.persistence.Jpa;
 
 public class CitizensLoader {
 
-    private List<Citizen> citizens;
+	private List<Citizen> citizens;
 
-    /** No tratamos las excepciones, deberíamos hacer un log con los errores **/
-    public static void main(String... args) throws IOException {
-	// new CitizensLoader().load((String) args[0], (String) args[1]);
-
-	// Por si hay problemas con la de arriba
-	new CitizensLoader().load("excel", "src/test/resources/test.xlsx");
-	// new CitizensLoader().load("texto", "src/test/resources/test.txt");
-    }
-
-    public void load(String formato, String filePath) throws IOException {
-	citizens = getReader(formato).readCitizens(filePath);
-	printCitizens(citizens);
-	// Comprobaciones (¿Alguno de los usuarios que he leido está ya en la
-	// base de datos?)
-	// Insertar en base de datos
-    }
-
-    protected List<Citizen> getCitizens() {
-	return citizens;
-    }
-
-    /**
-     * Crea y devuelve el reader adecuado
-     */
-    private CitizensReader getReader(String formato) {
-	if ("excel".equals(formato)) {
-	    return new ExcelCitizensReader();
-	} else if ("texto".equals(formato)) {
-	    return new TextCitizensReader();
+	public static void main(String... args) {
+		try {
+			new CitizensLoader().load((String) args[0], (String) args[1]);
+			// Por si hay problemas con la de arriba
+			// new CitizensLoader().load("excel", "src/test/resources/test.xlsx");
+			// new CitizensLoader().load("texto", "src/test/resources/test.txt");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	return null;
-    }
 
-    private void printCitizens(List<Citizen> citizens) {
-	for (Citizen citizen : citizens) {
-	    System.out.println(citizen);
+	public void load(String formato, String filePath) throws IOException {
+		citizens = getReader(formato).readCitizens(filePath);
+		printCitizens(citizens, filePath);
+		// Comprobaciones (¿Alguno de los usuarios que he leido está ya en la
+		// base de datos?)
+		// Insertar en base de datos
 	}
-    }
 
-    private void loadDB(List<Citizen> citizens) {
-	EntityManager EM = Jpa.getEntityManager();
-	EntityTransaction ET = EM.getTransaction();
-	ET.begin();
-	for (Citizen c : citizens) {
-	    EM.persist(c);
+	protected List<Citizen> getCitizens() {
+		return citizens;
 	}
-	ET.commit();
-    }
 
-    /** Comprobar para cada ciudadano si ya esta en la BD **/
-    public boolean isCitizenEqualTo(Citizen c) {
-	Long numero = (Long) Jpa.getEntityManager()
-		.createQuery("SELECT COUNT(c)" + " FROM CITIZEN C" + " WHERE C.DNI = ?1").setParameter(1, c.getDni())
-		.getSingleResult();
-	return numero != 0;
-    }
+	/**
+	 * Crea y devuelve el reader adecuado
+	 */
+	private CitizensReader getReader(String formato) {
+		if ("excel".equals(formato)) {
+			return new ExcelCitizensReader();
+		} else if ("texto".equals(formato)) {
+			return new TextCitizensReader();
+		}
+		return null;
+	}
+
+	private void printCitizens(List<Citizen> citizens, String filePath) {
+		System.out.println("Estos son los usuarios presentes en el fichero " + filePath + ":");
+		for (Citizen citizen : citizens) {
+			System.out.println(citizen);
+		}
+	}
+
+	private void loadDB(List<Citizen> citizens) {
+		EntityManager EM = Jpa.getEntityManager();
+		EntityTransaction ET = EM.getTransaction();
+		ET.begin();
+		for (Citizen c : citizens) {
+			EM.persist(c);
+		}
+		ET.commit();
+	}
+
+	/** Comprobar para cada ciudadano si ya esta en la BD **/
+	public boolean isCitizenEqualTo(Citizen c) {
+		Long numero = (Long) Jpa
+				.getEntityManager()
+				.createQuery(
+						"SELECT COUNT(c)" + " FROM CITIZEN C"
+								+ " WHERE C.DNI = ?1")
+				.setParameter(1, c.getDni()).getSingleResult();
+		return numero != 0;
+	}
 
 }
--- src/test/java/es/uniovi/asw/CitizensLoaderTest.java
+++ src/test/java/es/uniovi/asw/CitizensLoaderTest.java
@@ -3,7 +3,10 @@ package es.uniovi.asw;
 import static org.junit.Assert.*;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -11,8 +14,8 @@ import org.junit.Test;
 import es.uniovi.asw.modelos.Citizen;
 
 public class CitizensLoaderTest {
+	
     private CitizensLoader excel;
-    private CitizensLoader txt;
 
     @Before
     public void cargaExcel() throws IOException {
@@ -20,48 +23,54 @@ public class CitizensLoaderTest {
 	excel.load("excel", "src/test/resources/test.xlsx");
     }
 
-    @Before
-    public void cargaTxt() throws IOException {
-	txt = new CitizensLoader();
-	txt.load("texto", "src/test/resources/test.txt");
-    }
 
     // El suppressWarning es porque la clase Date que usamos al cargar la fecha
     // está obsoleta
     @SuppressWarnings("deprecation")
     @Test
     public void testParserExcel() {
-	int size = excel.getCitizens().size() - 1;
-	Citizen listExcel = excel.getCitizens().get(size);
-	// El usuario de control para comprobar que todo se carga correctamente
-	// será el último de la lista del excel
-	assertTrue(listExcel.getNombre().equals("Ana"));
-	assertTrue(listExcel.getApellidos().equals("Torres Pardo"));
-	assertTrue(listExcel.getEmail().equals("ana@example.com"));
-	Date date = new Date("01/01/1960");
-	// assertTrue(listExcel.getFechaNacimiento() == date);
-	assertTrue(listExcel.getDireccionPostal().equals("Av. De la Constitución 8"));
-	assertTrue(listExcel.getNacionalidad().equals("Español"));
-	assertTrue(listExcel.getDni().equals("09940449X"));
+		int size = excel.getCitizens().size() - 1;
+		Citizen listExcel = excel.getCitizens().get(size);
+		// El usuario de control para comprobar que todo se carga correctamente
+		// será el último de la lista del excel
+		assertTrue(listExcel.getNombre().equals("Ana"));
+		assertTrue(listExcel.getApellidos().equals("Torres Pardo"));
+		assertTrue(listExcel.getEmail().equals("ana@example.com"));
+		Date date = new Date("01/01/1960");
+		// assertTrue(listExcel.getFechaNacimiento() == date);
+		assertTrue(listExcel.getDireccionPostal().equals("Av. De la Constitución 8"));
+		assertTrue(listExcel.getNacionalidad().equals("Español"));
+		assertTrue(listExcel.getDni().equals("09940449X"));
     }
 
-    // El suppressWarning es porque la clase Date que usamos al cargar la fecha
-    // está obsoleta
-    @SuppressWarnings("deprecation")
     @Test
-    public void testParserTxt() {
-	int size = txt.getCitizens().size() - 1;
-	Citizen listTxt = txt.getCitizens().get(size);
-	// El usuario de control para comprobar que todo se carga correctamente
-	// será el último de la lista del txt
-	assertTrue(listTxt.getNombre().equals("Luis"));
-	assertTrue(listTxt.getApellidos().equals("López Fernando"));
-	assertTrue(listTxt.getEmail().equals("luis@example"));
-	Date date = new Date("02/03/1970");
-	// assertTrue(listTxt.getFechaNacimiento() == date);
-	assertTrue(listTxt.getDireccionPostal().equals("C/Real Oviedo 2"));
-	assertTrue(listTxt.getNacionalidad().equals("Español"));
-	assertTrue(listTxt.getDni().equals("19160962F"));
+    public void testCitizensLoaderFromTxt() throws IOException {
+    	CitizensLoader citizensLoader = new CitizensLoader();
+		citizensLoader.load("texto", "src/test/resources/test.txt");
+		List<Citizen> citizens = citizensLoader.getCitizens();
+		assertEquals(6, citizens.size());
+		
+		DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		
+		Citizen pablo = citizens.get(0);
+		assertEquals(pablo.getNombre(), "Pablo");
+		assertEquals(pablo.getApellidos(), "Alonso Gil");
+		assertEquals(pablo.getEmail(), "pablo@example.com");
+		String fechaNacimientoPablo = dateFormat.format(pablo.getFechaNacimiento());
+		assertEquals(fechaNacimientoPablo, "10/10/1985");
+		assertEquals(pablo.getDireccionPostal(), "Calle Mayor 2");
+		assertEquals(pablo.getNacionalidad(), "Noruega");
+		assertEquals(pablo.getDni(), "90500084Y");
+		
+		Citizen eva = citizens.get(5);
+		assertEquals(eva.getNombre(), "Eva");
+		assertEquals(eva.getApellidos(), "Belmonte Blanco");
+		assertEquals(eva.getEmail(), "eva@example.com");
+		String fechaNacimientoEva = dateFormat.format(eva.getFechaNacimiento());
+		assertEquals(fechaNacimientoEva, "02/05/1960");
+		assertEquals(eva.getDireccionPostal(), "Avenida del sur 5");
+		assertEquals(eva.getNacionalidad(), "Italiana");
+		assertEquals(eva.getDni(), "59120962S");
     }
 
 }
